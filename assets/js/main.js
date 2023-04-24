f507e221bdeb86e531e8bfa286a6d07dc9ee7782
Felipe1501 <felipebsantos1501@gmail.com>
2023-04-24 14:23:06 -0300

    separando o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 
 
 function convertPokemonToLi(pokemon){
@@ -23,18 +21,14 @@ function convertPokemonToLi(pokemon){
 const pokemonList = document.getElementById('pokemonList');
 
 
-fetch(url)
-    .then((response) =>  response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        
+
+    PokeAPI.getPokemons().then((pokemons) => {
+        const listItems = []
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
-            
+            listItems.push(convertPokemonToLi(pokemon))
         }
+        console.log(listItems);
     }) 
 
-    .catch((error) => console.error(error))
 
