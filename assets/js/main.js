ff696e80ad3b8033cda7874c6582d2bf4bb04985
Felipe1501 <felipebsantos1501@gmail.com>
2023-04-24 14:41:00 -0300

    utilizando função map js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,13 +22,17 @@ const pokemonList = document.getElementById('pokemonList');
 
 
 
-    PokeAPI.getPokemons().then((pokemons) => {
-        const listItems = []
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            listItems.push(convertPokemonToLi(pokemon))
-        }
-        console.log(listItems);
+    PokeAPI.getPokemons().then((pokemons = []) => {
+
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+
+        // const listItems = []
+        // for (let i = 0; i < pokemons.length; i++) {
+        //     const pokemon = pokemons[i];
+        //     listItems.push(convertPokemonToLi(pokemon))
+        // }
+        // console.log(listItems);
     }) 
 
 
