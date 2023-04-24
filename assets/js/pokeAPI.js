a15f3e467937bf0928e4391c7c972253ece9061f
Felipe1501 <felipebsantos1501@gmail.com>
2023-04-24 15:42:20 -0300

    convertendo o modelo do PokeApi para nosso modelo

--- assets/js/pokeAPI.js
+++ assets/js/pokeAPI.js
@@ -1,8 +1,25 @@
 const PokeAPI = {}
 
+function convertPokeAPIDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 PokeAPI.getPokemonDetail = (pokemon) => {
    return fetch(pokemon.url)
             .then((response) => response.json())
+            .then(convertPokeAPIDetailToPokemon)
 }
 
 PokeAPI.getPokemons = (offset = 0, limit = 10) => {
